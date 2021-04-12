fd117c61d5bb056426ec5629f9ed4251bbe3ac94
PhiSko <phillip.skowronek@siemens.com>
2021-04-12 09:28:33 +0200

    s

--- Casino_2/Casino_2.cpp
+++ Casino_2/Casino_2.cpp
@@ -1,20 +1,76 @@
-// Casino_2.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+// Casino.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
 //
-
 #include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+class Spiele
+{
+public:
+    Spiele();
+    ~Spiele();
+private:
+};
+Spiele::Spiele()
+{
+}
+Spiele::~Spiele()
+{
+}
+class Roulette : public Spiele
+{
+public:
+    Roulette();
+    ~Roulette();
+private:
+};
+Roulette::Roulette()
+{
+}
+Roulette::~Roulette()
+{
+}
+class Slotmachine : public Spiele
+{
+public:
+    Slotmachine();
+    ~Slotmachine();
+private:
+};
+Slotmachine::Slotmachine()
+{
+}
+Slotmachine::~Slotmachine()
+{
+}
+class BlackJack : public Spiele
+{
+public:
+    BlackJack();
+    ~BlackJack();
+private:
+};
+BlackJack::BlackJack()
+{
+}
+BlackJack::~BlackJack()
+{
+}
+class Spieler
+{
+public:
+    Spieler();
+    ~Spieler();
+private:
+};
+Spieler::Spieler()
+{
+}
+Spieler::~Spieler()
+{
 
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
